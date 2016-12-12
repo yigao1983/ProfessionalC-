ce75ef79eeac404ac94caf549ccc4f2474ded170
Yi Gao <yigao1983@gmail.com>
2016-12-11 19:06:30 -0600

    Add testfunctemp.cpp

--- testfunctemp.cpp
+++ testfunctemp.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
 template <typename T>
 int find(T &value, T *arr, int size)
 {
@@ -7,4 +12,34 @@ int find(T &value, T *arr, int size)
     }
   }
   return -1;
+}
+
+template <>
+int find(char * &value, char **arr, int size)
+{
+  for (int i = 0; i < size; i++) {
+    if (strcmp(arr[i], value) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int main(int argc, char **argv)
+{
+  int *arr;
+  
+  arr = new int [10];
+  
+  for (int i = 0; i < 10; i++) {
+    arr[i] = 2*i;
+  }
+  
+  int b = 8;
+  
+  cout << find(b, arr, 10) << endl;
+  
+  delete [] arr;
+  
+  return 0;
 }
